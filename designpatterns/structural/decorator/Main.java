9935edff2c1929076e639901dc80a30e55991185
Kamal Rajput <kamalrajput@Kamals-MacBook-Air.local>
2026-05-21 13:30:26 +0530

    Learned decorator design pattern

--- designpatterns/structural/decorator/Main.java
+++ designpatterns/structural/decorator/Main.java
@@ -14,5 +14,7 @@ public class Main {
         System.out.println("Total Cost: " + coffee.cost());
 
         System.out.println();
+
+        System.out.println("Added decorator design pattern");
     }
 }
